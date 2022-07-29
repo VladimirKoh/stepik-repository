719952fb11205986a18eed7ec0daf429ec237b93
VladimirKoh <vladimirrodionov97@icloud.com>
2022-07-29 11:38:46 +0300

    first-commit-02

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hellow world")
+
+
+class Hellow_world:
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self) -> str:
+        print(self.name)
+
+test = Hellow_world('Hello!')
+test.get_name()
