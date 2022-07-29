5a03b00de961a326aca30211fef3b2cc7c0f2a6c
Vladimir <109983400+VladimirKoh@users.noreply.github.com>
2022-07-29 15:00:51 +0300

    check-01

--- main.py
+++ main.py
@@ -2046,3 +2046,64 @@
 
 
 #mikheev_loh
+
+
+
+class ListMath:
+    def __init__(self, lst=None):
+        self.lst_math = lst if lst and type(lst) == list else []
+        self.lst_math = list(filter(lambda x: type(x) in (int, float), self.lst_math))
+
+    @staticmethod
+    def __verify_value(value):
+        if type(other) not in (int, float):
+            raise ArithmeticError('xyu')
+
+    def __add__(self, other):
+        self.__verify_value(other)
+        return ListMath([x + other for x in self.lst_math])
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        self.__verify_value(other)
+        return ListMath([x - other for x in self.lst_math])
+
+    def __rsub__(self, other):
+        return ListMath([other - x for x in self.lst_math])
+
+    def __mul__(self, other):
+        self.__verify_value(other)
+        return ListMath([x * other for x in self.lst_math])
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other)
+        self.__verify_value(other)
+        return ListMath([x / other for x in self.lst_math])
+
+    def __rtruediv__(self, other):
+        return ListMath([other / x for x in self.lst_math])
+
+    def __iadd__(self, other):
+        self.__verify_value(other)
+        self.lst_math = [x + other for x in self.lst_math]
+        return self
+
+    def __isub__(self, other):
+        self.__verify_value(other)
+        self.lst_math = [x - other for x in self.lst_math]
+        return self
+
+    def __imul__(self, other):
+        self.__verify_value(other)
+        self.lst_math = [x * other for x in self.lst_math]
+        return self
+    
+    def __itruediv__(self, other):
+        self.__verify_value(other)
+        self.lst_math = [x / other for x in self.lst_math]
+        return self
+    
