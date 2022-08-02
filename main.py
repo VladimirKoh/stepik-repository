3ae84b6b22e9e2568f428f0e78619ff5ad9b3ea4
VladimirKoh <vladimirrodionov97@icloud.com>
2022-08-02 09:05:00 +0300

    check 06 2file

--- main.py
+++ main.py
@@ -2286,48 +2286,52 @@
 #zadaniye 3.4.8
 
 
-class Item:
-    def __init__(self, name, money):
-        self.name = name
-        self.money = money
+# class Item:
+#     def __init__(self, name, money):
+#         self.name = name
+#         self.money = money
+#
+#     def __add__(self, other):
+#         sc = other
+#         if isinstance(other, Item):
+#             sc = other.money
+#
+#         out = self.money + sc
+#         return out
+#
+#     def __radd__(self, other):
+#         return self + other
+#
+#
+# class Budget:
+#     def __init__(self):
+#         self.list_items = list()
+#
+#     def add_item(self, it):
+#         self.list_items.append(it)
+#
+#     def remove_item(self, indx):
+#         self.list_items.pop(indx)
+#
+#     def get_items(self):
+#         return self.list_items
+#
+#
+# my_budget = Budget()
+# my_budget.add_item(Item("Курс по Python ООП", 2000))
+# my_budget.add_item(Item("Курс по Django", 5000.01))
+# my_budget.add_item(Item("Курс по NumPy", 0))
+# my_budget.add_item(Item("Курс по C++", 1500.10))
+#
+# # вычисление общих расходов
+# s = 0
+# for x in my_budget.get_items():
+#     s = s + x
+#     print(x)
+#     print(s)
 
-    def __add__(self, other):
-        sc = other
-        if isinstance(other, Item):
-            sc = other.money
-
-        out = self.money + sc
-        return out
-
-    def __radd__(self, other):
-        return self + other
 
 
-class Budget:
-    def __init__(self):
-        self.list_items = list()
-
-    def add_item(self, it):
-        self.list_items.append(it)
-
-    def remove_item(self, indx):
-        self.list_items.pop(indx)
-
-    def get_items(self):
-        return self.list_items
-
-
-my_budget = Budget()
-my_budget.add_item(Item("Курс по Python ООП", 2000))
-my_budget.add_item(Item("Курс по Django", 5000.01))
-my_budget.add_item(Item("Курс по NumPy", 0))
-my_budget.add_item(Item("Курс по C++", 1500.10))
-
-# вычисление общих расходов
-s = 0
-for x in my_budget.get_items():
-    s = s + x
-    print(x)
-    print(s)
-
+#zadaniye 3.4.9
+#in main2.py
 
